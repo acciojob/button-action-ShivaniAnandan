9bc5c05d6574dde44088a9bbd8e61af10201d43e
Shivani Anandan <shivanianandhan28@gmail.com>
2023-07-25 22:22:57 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  
+  let [text,setText]=useState("");
+  function showText(){
+    setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
+
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+       <p id="para" className="hide show">{text}</p>
+      <button id="click" onClick={showText}>Show Text</button>
     </div>
   );
 }
